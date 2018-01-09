cb8993e58013fc67f14c872b16f65c265257459c
Shou Hao, Zhao <j5201314peter@gmail.com>
2018-01-09 11:47:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-  echo "Line Bot";
+  $json_str = file_get_contents('php://input');
+  $json_obj = json_decode($json_str);
+
+$myfile = fopen("log.txt','w+") or die("Unable to open file!");
+fwrite($myfile, "\xEF\xBB\xBf".$json_str);
+fclose($myfile);
 ?>
