f2e68693d407356ef77f3e7cb881d6093e7b7065
Shou Hao, Zhao <j5201314peter@gmail.com>
2018-01-09 11:48:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   $json_str = file_get_contents('php://input');
   $json_obj = json_decode($json_str);
 
-$myfile = fopen("log.txt\","w+") or die("Unable to open file!");
+$myfile = fopen("log.txt","w+") or die("Unable to open file!");
 fwrite($myfile, "\xEF\xBB\xBf".$json_str);
 fclose($myfile);
 ?>
